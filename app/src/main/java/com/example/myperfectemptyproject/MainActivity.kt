f6076a0e2d46026f3bc2b5286c8707c6b705762a
Eugene <e.shuvagin.vironit@vironit.ru>
2019-12-16 18:48:35 +0300

    add binding

--- app/src/main/java/com/example/myperfectemptyproject/MainActivity.kt
+++ app/src/main/java/com/example/myperfectemptyproject/MainActivity.kt
@@ -2,16 +2,16 @@ package com.example.myperfectemptyproject
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.example.myperfectemptyproject.ui.main.MainFragment
+import androidx.databinding.DataBindingUtil
+import com.example.myperfectemptyproject.databinding.MainActivityBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var binding: MainActivityBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        if (savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance()).commitNow()
-        }
+        binding = DataBindingUtil.setContentView(this, R.layout.main_activity)
+
     }
 }
--- app/src/main/java/com/example/myperfectemptyproject/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/myperfectemptyproject/ui/main/MainFragment.kt
@@ -7,26 +7,28 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProviders
 import com.example.myperfectemptyproject.R
+import com.example.myperfectemptyproject.databinding.MainFragmentBinding
+import com.example.myperfectemptyproject.di.injector
+import com.example.myperfectemptyproject.utils.viewModel
 
 class MainFragment : Fragment() {
 
-    companion object {
-        fun newInstance() = MainFragment()
+    lateinit var binding : MainFragmentBinding
+    private val viewmodel by viewModel {
+        injector.mainViewModel
     }
-
-    private lateinit var viewModel: MainViewModel
 
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+        binding = MainFragmentBinding.inflate(layoutInflater,container,false)
+        return binding.root
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        viewModel = ViewModelProviders.of(this).get(MainViewModel::class.java)
-        // TODO: Use the ViewModel
+
     }
 }
