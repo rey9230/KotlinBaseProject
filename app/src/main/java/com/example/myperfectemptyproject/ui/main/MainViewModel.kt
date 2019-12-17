5c4cf32b4e1d7b82c3ccc0c4b4606c54a0dcefa7
Eugene <e.shuvagin.vironit@vironit.ru>
2019-12-17 14:21:14 +0300

    clean up

--- app/src/main/java/com/example/myperfectemptyproject/ui/main/MainViewModel.kt
+++ app/src/main/java/com/example/myperfectemptyproject/ui/main/MainViewModel.kt
@@ -2,7 +2,10 @@ package com.example.myperfectemptyproject.ui.main
 
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.viewModelScope
+import com.example.myperfectemptyproject.data.source.remote.ApiErrorHandle
 import com.example.myperfectemptyproject.ui.main.domain.usecase.UseCase
 import javax.inject.Inject
 import kotlinx.coroutines.launch
@@ -12,11 +15,18 @@ class MainViewModel @Inject constructor(
     private val useCase: UseCase
 ) : AndroidViewModel(application) {
 
+    private val _isLoading = MutableLiveData<Boolean>(false)
+    val isLoading: LiveData<Boolean> = _isLoading
+    private val _errorMessage = MutableLiveData<String?>(null)
+    val errorMessage: LiveData<String?> = _errorMessage
+
     init {
         viewModelScope.launch {
-            useCase.execute().onSuccess {
-            }.onFailure {
-            }
+            useCase.execute()
+                .onSuccess {
+                }.onFailure {
+                    _errorMessage.value = ApiErrorHandle.traceErrorException(it).getErrorMessage()
+                }
         }
     }
 }
