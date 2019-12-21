f19d5083082f08b8d99173e05e8773232ea9f7a2
i30mb1 <fate.i30mb1@gmail.com>
2019-12-21 13:07:19 +0300

    one big file converted to kotlin DSL

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,60 +1,70 @@
-apply plugin: "com.android.application"
-apply plugin: "kotlin-android"
-apply plugin: "kotlin-android-extensions"
-apply plugin: "kotlin-kapt"
-apply plugin: "androidx.navigation.safeargs.kotlin"
+@file:Suppress("SpellCheckingInspection")
+
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    id("com.android.application")
+    kotlin("android")
+    kotlin("android.extensions")
+    kotlin("kapt")
+    id("androidx.navigation.safeargs.kotlin")
+}
 
 android {
-    compileSdkVersion = 29
+    compileSdkVersion(29)
     buildToolsVersion = "29.0.2"
     defaultConfig {
-        applicationId = "com.example.myperfectemptyproject"
         minSdkVersion(23)
         targetSdkVersion(29)
         versionCode = 1
         versionName = "1.0"
+//        multiDexEnabled = true
+        applicationId = "com.example.myperfectemptyproject"
         vectorDrawables.useSupportLibrary = true
-        project.ext.set("archivesBaseName","i30mb1-" + defaultConfig.versionName)
-   }
+        setProperty("archivesBaseName", "$applicationId-v$versionName($versionCode)")
+    }
     sourceSets {
-        main.java.srcDirs += "src/main/kotlin"
+        getByName("main").java.srcDirs("src/main/kotlin")
     }
     buildTypes {
-        release {
-            minifyEnabled = false
-//            shrinkResources true
-            proguardFiles (getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+        getByName("release") {
+            isMinifyEnabled = false
+            isShrinkResources = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
-        sourceCompatibility JavaVersion.VERSION_1_8
-        targetCompatibility JavaVersion.VERSION_1_8
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
     }
-    kotlinOptions {
-        jvmTarget = "1.8"
-        freeCompilerArgs = ["-Xallow-result-return-type"]
+    tasks.withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = JavaVersion.VERSION_1_8.toString()
+        kotlinOptions.freeCompilerArgs = listOf("-Xallow-result-return-type")
     }
     dataBinding {
-        enabled = true
+        isEnabled = true
     }
     androidExtensions {
-        experimental = true
+        isExperimental = true
     }
 }
 
 dependencies {
-    implementation (fileTree(dir: "libs", include: ["*.jar"]))
+    implementation(fileTree(mapOf("dir" to "libs", "include" to listOf("*.jar"))))
     // Starting with Kotlin 1.1.2, the dependencies with group org.jetbrains.kotlin are by default resolved with the version taken from the applied plugin
-    // implementation(kotlin("stdlib-jdk7"))
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.60")
+//    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.60")
+    implementation(kotlin("stdlib-jdk7"))
     implementation("androidx.appcompat:appcompat:1.1.0")
     implementation("androidx.core:core-ktx:1.1.0")
     implementation("androidx.constraintlayout:constraintlayout:1.1.3")
-    implementation("com.google.android.material:material:1.2.0-alpha01")
+    implementation("com.google.android.material:material:1.2.0-alpha03")
 
     // --- Coroutines ---
-    def coroutines = "1.3.2"
-    def coroutinesExt = "2.2.0-alpha01"
+    val coroutines = "1.3.2"
+    val coroutinesExt = "2.2.0-alpha01"
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:$coroutines")
     // для тестирования и ...
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:$coroutines")
@@ -69,17 +79,17 @@ dependencies {
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-play-services:$coroutines")
 
     // --- Dagger ---
-    def dagger = "2.25.2"
+    val dagger = "2.25.2"
     implementation("com.google.dagger:dagger:$dagger")
     kapt("com.google.dagger:dagger-compiler:$dagger")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
 
     // --- Coil ---
-    def coil = "0.8.0"
+    val coil = "0.8.0"
     implementation("io.coil-kt:coil:$coil")
 
     //  --- Navigation ---
-    def navigation = "2.2.0-beta01"
+    val navigation = "2.2.0-beta01"
     // Fragment.findNavController + Fragment.navArgs
     implementation("androidx.navigation:navigation-fragment-ktx:$navigation")
     // setupActionBarWithNavController + setupWithNavController
@@ -93,17 +103,17 @@ dependencies {
     implementation("androidx.activity:activity-ktx:1.1.0-beta01")
 
     // --- Retrofit ---
-    def retrofit = "2.6.2"
+    val retrofit = "2.6.2"
     implementation("com.squareup.retrofit2:retrofit:$retrofit")
     implementation("com.squareup.retrofit2:converter-moshi:$retrofit")
     // --- Moshi ---
-    def moshi = "1.9.2"
+    val moshi = "1.9.2"
     implementation("com.squareup.moshi:moshi:$moshi")
     // generate code in compile time instead of using runtime reflection
     kapt("com.squareup.moshi:moshi-kotlin-codegen:$moshi")
 
     // --- Room ---
-    def room = "2.2.2"
+    val room = "2.2.2"
     implementation("androidx.room:room-runtime:$room")
     kapt("androidx.room:room-compiler:$room")
     // kotlin Extensions and Coroutines support for Room
