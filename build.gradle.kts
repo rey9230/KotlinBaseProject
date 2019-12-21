9808a7941367c85045bc368ddcddc66997ba3631
i30mb1 <fate.i30mb1@gmail.com>
2019-12-21 11:58:00 +0300

    2 file converted to kotlin DSL

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,13 +2,13 @@
 
 buildscript {
     repositories {
+        maven("https://maven.google.com")
         google()
         jcenter()
-        
     }
     dependencies {
         classpath("com.android.tools.build:gradle:3.5.3")
-        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.3.60")
+        classpath(kotlin("gradle-plugin", version = "1.3.60"))
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:2.2.0-alpha03")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
@@ -22,6 +22,6 @@ allprojects {
     }
 }
 
-task clean(type: Delete) {
-    delete rootProject.buildDir
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
