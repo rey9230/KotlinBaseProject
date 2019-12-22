f968490081660e407be8fb238fe12970af1183a8
i30mb1 <fate.i30mb1@gmail.com>
2019-12-22 14:10:03 +0300

    added signed confiq

--- buildSrc/src/main/kotlin/Dependencies.kt
+++ buildSrc/src/main/kotlin/Dependencies.kt
@@ -11,6 +11,8 @@ object Versions {
     const val gradle = "3.5.3"
     const val kotlin = "1.3.60"
     const val appcompat = "1.1.0"
+
+    const val safeArgs = "2.2.0-alpha03"
 }
 
 object Libs {
