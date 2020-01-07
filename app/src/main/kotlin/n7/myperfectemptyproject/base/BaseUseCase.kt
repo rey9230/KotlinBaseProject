89416a0a2334dd44a654ba88ad3505b23cf52425
EXT02D20804 <evgeniy.shuvagin@turkcell.com.tr>
2020-01-07 14:52:36 +0300

    added BaseUseCase

---
+++ app/src/main/kotlin/n7/myperfectemptyproject/base/BaseUseCase.kt
@@ -0,0 +1,7 @@
+package n7.myperfectemptyproject.base
+
+abstract class BaseUseCase<Type, in Params> constructor() where Type : Any {
+
+    abstract suspend fun execute(params: Params? = null): Result<Type>
+
+}
--- app/src/main/kotlin/n7/myperfectemptyproject/ui/main/domain/usecase/UseCase.kt
+++ app/src/main/kotlin/n7/myperfectemptyproject/ui/main/domain/usecase/UseCase.kt
@@ -1,17 +1,20 @@
 package n7.myperfectemptyproject.ui.main.domain.usecase
 
-import javax.inject.Inject
+import androidx.lifecycle.LiveData
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
+import n7.myperfectemptyproject.base.BaseUseCase
 import n7.myperfectemptyproject.data.source.local.db.AppDatabase
+import n7.myperfectemptyproject.data.source.local.model.LocalModel
 import n7.myperfectemptyproject.data.source.remote.RepositoryImpl
+import javax.inject.Inject
 
 class UseCase @Inject constructor(
     private val repositoryImpl: RepositoryImpl,
     private val appDatabase: AppDatabase
-) {
+) : BaseUseCase<LiveData<List<LocalModel>>, Any>() {
 
-    suspend fun execute() = withContext(Dispatchers.IO) {
+    override suspend fun execute(params: Any?) = withContext(Dispatchers.IO) {
         kotlin.runCatching {
             repositoryImpl
             appDatabase.myDao.getAll()
