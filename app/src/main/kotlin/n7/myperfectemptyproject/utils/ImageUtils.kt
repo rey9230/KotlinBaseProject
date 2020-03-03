912dbce27dfe803e8746e7ed3f8946d7e4f7cbfc
EXT02D20804 <evgeniy.shuvagin@turkcell.com.tr>
2020-03-03 22:16:48 +0300

    add crossfade (aka crossfade animation with durationMillis milliseconds when a request completes successfully)

--- app/src/main/kotlin/n7/myperfectemptyproject/utils/ImageUtils.kt
+++ app/src/main/kotlin/n7/myperfectemptyproject/utils/ImageUtils.kt
@@ -3,11 +3,12 @@ package n7.myperfectemptyproject.utils
 import android.widget.ImageView
 import androidx.databinding.BindingAdapter
 import coil.api.load
-import coil.transform.CircleCropTransformation
+import coil.transform.RoundedCornersTransformation
 
 @BindingAdapter("loadImageUrl")
 fun ImageView.loadImageUrl(url: String?) {
     this.load(url) {
-        transformations(CircleCropTransformation())
+        crossfade(true)
+        transformations(RoundedCornersTransformation(10F))
     }
 }
