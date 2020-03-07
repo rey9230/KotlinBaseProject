a3df8906cb3771ef2b4fd82bdbdf34d386a00ece
i30mb1 <fate.i30mb1@gmail.com>
2020-03-07 14:53:58 +0300

    add delay for further testing coroutines (tests should skip all delays)

--- app/src/main/kotlin/n7/myperfectemptyproject/ui/main/domain/usecase/GetUsersFromRemoteStoreUseCase.kt
+++ app/src/main/kotlin/n7/myperfectemptyproject/ui/main/domain/usecase/GetUsersFromRemoteStoreUseCase.kt
@@ -3,6 +3,7 @@ package n7.myperfectemptyproject.ui.main.domain.usecase
 import javax.inject.Inject
 import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
 import kotlinx.coroutines.withContext
 import n7.myperfectemptyproject.data.source.RepositoryImpl
 import n7.myperfectemptyproject.data.source.local.db.UsersDao
@@ -15,6 +16,7 @@ class GetUsersFromRemoteStoreUseCase @Inject constructor(
 
     suspend fun execute(count: Int) = withContext(ioDispatcher) {
         kotlin.runCatching {
+            delay(3000)
             repositoryImpl.getRemoteUsers(count)
         }
     }
