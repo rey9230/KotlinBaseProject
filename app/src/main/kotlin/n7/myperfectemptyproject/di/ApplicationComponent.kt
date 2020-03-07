ba779c8036696a6561fa175123bf602522d8fce0
i30mb1 <fate.i30mb1@gmail.com>
2020-03-07 14:53:13 +0300

    add comments

--- app/src/main/kotlin/n7/myperfectemptyproject/di/ApplicationComponent.kt
+++ app/src/main/kotlin/n7/myperfectemptyproject/di/ApplicationComponent.kt
@@ -3,16 +3,18 @@ package n7.myperfectemptyproject.di
 import android.app.Application
 import dagger.BindsInstance
 import dagger.Component
-import javax.inject.Singleton
 import n7.myperfectemptyproject.ui.main.MainViewModel
+import javax.inject.Singleton
 
 @Singleton
-@Component(modules = [
-    ApplicationModule::class,
-    RetrofitModule::class,
-    RepositoryModule::class,
-    AssistedInjectModule::class
-])
+@Component(
+    modules = [
+        ApplicationModule::class,
+        RetrofitModule::class,
+        RepositoryModule::class,
+        AssistedInjectModule::class
+    ]
+)
 interface ApplicationComponent {
 
     @Component.Factory
@@ -21,7 +23,9 @@ interface ApplicationComponent {
         fun create(@BindsInstance applicationContext: Application): ApplicationComponent
     }
 
-//    val mainViewModel: MainViewModel
+    // get simple viewModel instance
+    val mainViewModel: MainViewModel
 
+    // get viewModel instance with param's
     val mainViewModelFactory: MainViewModel.Factory
 }
