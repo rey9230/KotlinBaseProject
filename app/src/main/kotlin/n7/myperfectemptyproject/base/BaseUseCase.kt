ddf2a21c1bb43e79b6a8013a08965ef8dad3efbb
i30mb1 <fate.i30mb1@gmail.com>
2020-03-08 20:31:08 +0300

    slightly improve BaseUseCase

--- app/src/main/kotlin/n7/myperfectemptyproject/base/BaseUseCase.kt
+++ app/src/main/kotlin/n7/myperfectemptyproject/base/BaseUseCase.kt
@@ -2,5 +2,5 @@ package n7.myperfectemptyproject.base
 
 abstract class BaseUseCase<Type, in Params> constructor() where Type : Any {
 
-    abstract suspend fun execute(params: Params? = null): Result<Type>
+    abstract suspend operator fun invoke(params: Params? = null): Type
 }
