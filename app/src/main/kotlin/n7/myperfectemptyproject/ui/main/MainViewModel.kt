37326bb45d0a15357bc571d0b88973339d027426
i30mb1 <fate.i30mb1@gmail.com>
2020-03-09 22:48:25 +0300

    delete this syka method

--- app/src/main/kotlin/n7/myperfectemptyproject/ui/main/MainViewModel.kt
+++ app/src/main/kotlin/n7/myperfectemptyproject/ui/main/MainViewModel.kt
@@ -33,19 +33,3 @@ class MainViewModel @AssistedInject constructor(
         }
     }
 }
-
-// does this class better than simple _isLoading???
-@Suppress("DataClassPrivateConstructor")
-data class LoadingState private constructor(val status: Status, val msg: String? = null) {
-    companion object {
-        val LOADED = LoadingState(Status.SUCCESS)
-        val LOADING = LoadingState(Status.RUNNING)
-        fun error(msg: String?) = LoadingState(Status.FAILED, msg)
-    }
-
-    enum class Status {
-        RUNNING,
-        SUCCESS,
-        FAILED
-    }
-}
