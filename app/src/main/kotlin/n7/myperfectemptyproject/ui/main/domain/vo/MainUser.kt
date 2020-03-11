943e7f4b05453cf553a2a1dfd71c23d4184c0fd1
i30mb1 <fate.i30mb1@gmail.com>
2020-03-11 23:48:53 +0300

    create UserVO for dataBinding

--- app/src/main/kotlin/n7/myperfectemptyproject/ui/main/domain/vo/MainUser.kt
+++
@@ -1,4 +0,0 @@
-package n7.myperfectemptyproject.ui.main.domain.vo
-
-// Writing clean models using extensions
-class MainUser
---
+++ app/src/main/kotlin/n7/myperfectemptyproject/ui/main/domain/vo/UserVO.kt
@@ -0,0 +1,37 @@
+package n7.myperfectemptyproject.ui.main.domain.vo
+
+import androidx.databinding.BaseObservable
+import androidx.databinding.Bindable
+import n7.myperfectemptyproject.BR
+
+// Writing clean models using extensions
+class UserVO : BaseObservable() {
+
+    @get:Bindable
+    var firstName: String = ""
+    set(value) {
+        field = value
+        notifyPropertyChanged(BR.firstName)
+    }
+
+    @get:Bindable
+    var lastName: String = ""
+    set(value) {
+        field = value
+        notifyPropertyChanged(BR.lastName)
+    }
+
+    @get:Bindable
+    var date : String =""
+    set(value) {
+        field = value
+        notifyPropertyChanged(BR.date)
+    }
+
+    @get:Bindable
+    var pictureUrl: String =""
+    set(value) {
+        field = value
+        notifyPropertyChanged(BR.pictureUrl)
+    }
+}
