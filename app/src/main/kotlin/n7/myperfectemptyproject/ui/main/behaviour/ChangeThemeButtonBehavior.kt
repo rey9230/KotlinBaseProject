5c4fa0a1ce9e2e4faf06a6782cd343a4efa701cc
i30mb1 <fate.i30mb1@gmail.com>
2020-03-13 00:41:06 +0300

    optimize my animation behaviour

--- app/src/main/kotlin/n7/myperfectemptyproject/ui/main/behaviour/ChangeThemeButtonBehavior.kt
+++ app/src/main/kotlin/n7/myperfectemptyproject/ui/main/behaviour/ChangeThemeButtonBehavior.kt
@@ -1,16 +1,18 @@
 package n7.myperfectemptyproject.ui.main.behaviour
 
+import android.animation.ObjectAnimator
 import android.content.Context
 import android.util.AttributeSet
 import android.view.View
 import androidx.coordinatorlayout.widget.CoordinatorLayout
-import androidx.core.math.MathUtils
 import androidx.core.view.ViewCompat
 import com.google.android.material.button.MaterialButton
 
-// ух блять я ёбаный далбаёб 20 минут думал де ошибка а она тута, забыл вторичный конструктор переопределить :(((((((((((((
-// ууууууууууу блять так ещё нужно в xml полный путь указывать, или эта шлюха пишет что attributeSet `Could not inflate Behavior subclass`
-internal class ChangeThemeButtonBehavior(context: Context, attributeSet: AttributeSet? = null) : CoordinatorLayout.Behavior<MaterialButton>(context, attributeSet) {
+internal class ChangeThemeButtonBehavior(context: Context, attributeSet: AttributeSet? = null) :
+    CoordinatorLayout.Behavior<MaterialButton>(context, attributeSet) {
+
+    var isHiding = false
+    var oa: ObjectAnimator? = null
 
     override fun onStartNestedScroll(
         coordinatorLayout: CoordinatorLayout,
@@ -32,11 +34,28 @@ internal class ChangeThemeButtonBehavior(context: Context, attributeSet: Attribu
         consumed: IntArray,
         type: Int
     ) {
-        child.translationX = MathUtils.clamp(
-            child.translationX + dy,
-            0f,
-            child.width.toFloat() + child.paddingEnd
-        )
+        if (dy > 0 && !isHiding) hiding(child)
+        if (dy < 0 && isHiding) showing(child)
         super.onNestedPreScroll(coordinatorLayout, child, target, dx, dy, consumed, type)
+    }
+
+    private fun hiding(target: View) {
+        isHiding = true
+        oa = ObjectAnimator.ofFloat(
+            target,
+            View.TRANSLATION_X,
+            -target.width.toFloat() - target.paddingEnd
+        ).apply {
+            duration = 300
+        }
+        oa?.start()
+    }
+
+    private fun showing(target: View) {
+        isHiding = false
+        oa = ObjectAnimator.ofFloat(target, View.TRANSLATION_X, 0f).apply {
+            duration = 300
+        }
+        oa?.start()
     }
 }
