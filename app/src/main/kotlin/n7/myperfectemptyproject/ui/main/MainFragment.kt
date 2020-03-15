36987ba2174d33082a996d9db0cd74332d0707d1
i30mb1 <fate.i30mb1@gmail.com>
2020-03-15 22:16:25 +0300

    move pressBack to util class

--- app/src/main/kotlin/n7/myperfectemptyproject/ui/main/MainFragment.kt
+++ app/src/main/kotlin/n7/myperfectemptyproject/ui/main/MainFragment.kt
@@ -14,17 +14,13 @@ import n7.myperfectemptyproject.R
 import n7.myperfectemptyproject.databinding.MainFragmentBinding
 import n7.myperfectemptyproject.di.injector
 import n7.myperfectemptyproject.ui.ErrorDialogDirections
+import n7.myperfectemptyproject.utils.setOnBackPressExit
 import n7.myperfectemptyproject.utils.showSnackbar
 import n7.myperfectemptyproject.utils.viewModelWithSavedStateHandle
 
 class MainFragment : Fragment(R.layout.main_fragment) {
 
-    companion object {
-        const val MILLIS_FOR_EXIT = 2000L
-    }
-
     private lateinit var binding: MainFragmentBinding
-    private var finishActivity = false
     private val viewModel: MainViewModel by viewModelWithSavedStateHandle { injector.mainViewModelFactory }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
@@ -37,7 +33,7 @@ class MainFragment : Fragment(R.layout.main_fragment) {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        setupOnBackPressedAction()
+        setOnBackPressExit()
         setupListAdapter()
         setupErrorMessage()
     }
@@ -68,18 +64,5 @@ class MainFragment : Fragment(R.layout.main_fragment) {
             viewTreeObserver.addOnPreDrawListener { startPostponedEnterTransition(); true }
         }
         viewModel.getUsers.observe(viewLifecycleOwner, usersListAdapter::submitList)
-    }
-
-    // handle back press action for this fragment
-    private fun setupOnBackPressedAction() {
-        requireActivity().onBackPressedDispatcher.addCallback(this) {
-            if (finishActivity) {
-                requireActivity().finish()
-            } else {
-                finishActivity = true
-                view?.showSnackbar(getString(R.string.all_press_again_for_exit))
-                lifecycleScope.launch { delay(MILLIS_FOR_EXIT); finishActivity = false }
-            }
-        }
     }
 }
---
+++ app/src/main/kotlin/n7/myperfectemptyproject/utils/Util.kt
@@ -0,0 +1,22 @@
+package n7.myperfectemptyproject.utils
+
+import androidx.activity.addCallback
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.lifecycleScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import n7.myperfectemptyproject.R
+
+fun Fragment.setOnBackPressExit() {
+    val millisForExit = 2000L
+    var killActivity = false
+    requireActivity().onBackPressedDispatcher.addCallback(this) {
+        if (killActivity) {
+            requireActivity().finish()
+        } else {
+            killActivity = true
+            view?.showSnackbar(getString(R.string.all_press_again_for_exit))
+            lifecycleScope.launch { delay(millisForExit); killActivity = false }
+        }
+    }
+}
