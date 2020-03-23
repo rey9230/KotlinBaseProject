60782332b399470eec654ae495fbcd1f67d88183
i30mb1 <fate.i30mb1@gmail.com>
2020-03-23 11:36:45 +0300

    add annotation

--- app/src/test/kotlin/n7/CoroutineTestRule.kt
+++ app/src/test/kotlin/n7/CoroutineTestRule.kt
@@ -11,6 +11,11 @@ fun CoroutineTestRule.runBlockingTest(block: suspend TestCoroutineScope.() -> Un
     testDispatcher.runBlockingTest(block)
 }
 
+/**
+ * This class is a unit test rule which watches for tests starting and finishing.
+ * It contains a reference to a TestCoroutineDispatcher, and as tests are starting and stopping it overrides the default Dispatchers.Main
+ * dispatcher and replaces the default with our test dispatcher.
+ */
 @ExperimentalCoroutinesApi
 class CoroutineTestRule(
     val testDispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()
