3614baae00439d18ca939858c3144eb6b1e8c6da
i30mb1 <fate.i30mb1@gmail.com>
2020-04-16 14:54:14 +0300

    clean up

--- app/src/androidTest/java/n7/myperfectemptyproject/ui/main/MainFragmentTest.kt
+++ app/src/androidTest/java/n7/myperfectemptyproject/ui/main/MainFragmentTest.kt
@@ -1,15 +1,8 @@
 package n7.myperfectemptyproject.ui.main
 
-import androidx.test.espresso.Espresso.onView
-import androidx.test.espresso.action.ViewActions.click
-import androidx.test.espresso.assertion.ViewAssertions.matches
-import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
-import androidx.test.espresso.matcher.ViewMatchers.withId
-import androidx.test.espresso.matcher.ViewMatchers.withText
-import androidx.test.rule.ActivityTestRule
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.rule.ActivityTestRule
 import n7.myperfectemptyproject.MainActivity
-import n7.myperfectemptyproject.R
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
@@ -19,9 +12,9 @@ import org.junit.runner.RunWith
 @RunWith(AndroidJUnit4::class) // local runner that handle all test in class
 class MainFragmentTest {
 
-    @Rule
-    @JvmField
-    var activityRule: ActivityTestRule<MainActivity> = ActivityTestRule(MainActivity::class.java)
+    @get:Rule
+    var activityRule: ActivityTestRule<MainActivity> =
+        ActivityTestRule(MainActivity::class.java) // handles Activity lifecycles
 
     @Before
     fun setUp() {
@@ -31,11 +24,11 @@ class MainFragmentTest {
     fun tearDown() {
     }
 
-    @Test
+    @Test(timeout = 1000)
     fun clickOnButton() {
-        onView(withId(R.id.b_test)).check(matches(withText("BUTTON")))
-        onView(withId(R.id.b_test)).perform(click())
-        onView(withId(R.id.b_test)).check(matches(withText("test")))
-        onView(withId(R.id.b_test)).check(matches(isDisplayed()))
+        // onView(withId(R.id.b_test)).check(matches(withText("BUTTON")))
+        // onView(withId(R.id.b_test)).perform(click())
+        // onView(withId(R.id.b_test)).check(matches(withText("test")))
+        // onView(withId(R.id.b_test)).check(matches(isDisplayed()))
     }
 }
