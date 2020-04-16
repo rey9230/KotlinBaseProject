6b8aa4998ace996ff79b2274cee8d4c5d8d948aa
i30mb1 <fate.i30mb1@gmail.com>
2020-04-16 14:19:35 +0300

    add Annotation for my androidTest

--- app/src/androidTest/java/n7/myperfectemptyproject/ui/main/MainFragmentTest.kt
+++ app/src/androidTest/java/n7/myperfectemptyproject/ui/main/MainFragmentTest.kt
@@ -1,13 +1,22 @@
 package n7.myperfectemptyproject.ui.main
 
+import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.action.ViewActions.click
+import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers.isDisplayed
+import androidx.test.espresso.matcher.ViewMatchers.withId
+import androidx.test.espresso.matcher.ViewMatchers.withText
 import androidx.test.rule.ActivityTestRule
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import n7.myperfectemptyproject.MainActivity
 import n7.myperfectemptyproject.R
 import org.junit.After
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
+import org.junit.runner.RunWith
 
+@RunWith(AndroidJUnit4::class) // local runner that handle all test in class
 class MainFragmentTest {
 
     @Rule
