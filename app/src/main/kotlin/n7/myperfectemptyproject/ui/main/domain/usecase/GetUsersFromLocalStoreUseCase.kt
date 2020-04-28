66c2397624ba8dd524aa89f361c1e44374259d58
i30mb1 <fate.i30mb1@gmail.com>
2020-04-28 17:55:45 +0300

    move UseCase work on WorkerThread

--- app/src/main/kotlin/n7/myperfectemptyproject/ui/main/domain/usecase/GetUsersFromLocalStoreUseCase.kt
+++ app/src/main/kotlin/n7/myperfectemptyproject/ui/main/domain/usecase/GetUsersFromLocalStoreUseCase.kt
@@ -2,17 +2,20 @@ package n7.myperfectemptyproject.ui.main.domain.usecase
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.Transformations
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
 import n7.myperfectemptyproject.data.source.local.db.UsersDao
 import n7.myperfectemptyproject.ui.main.domain.adapter.toVo
 import n7.myperfectemptyproject.ui.main.domain.vo.VOUser
 import javax.inject.Inject
 
 class GetUsersFromLocalStoreUseCase @Inject constructor(
-    private val userDao: UsersDao
+    private val userDao: UsersDao,
+    private val ioDispatcher: CoroutineDispatcher
 ) {
 
-    suspend operator fun invoke(): LiveData<List<VOUser>> {
-        return Transformations.map(userDao.getAllByLiveData()) {
+    suspend operator fun invoke(): LiveData<List<VOUser>> = withContext(ioDispatcher) {
+        Transformations.map(userDao.getAllByLiveData()) {
             it.map { user -> user.toVo() }
         }
     }
