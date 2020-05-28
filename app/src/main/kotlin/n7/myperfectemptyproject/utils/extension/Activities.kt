a0e5f8f1ce6f4300c5e8eef8779f58cd9f3889e6
i30mb1 <fate.i30mb1@gmail.com>
2020-05-28 14:46:49 +0300

    add method to share files across apps https://android-tools.ru/coding/delimsya-fajlami-v-android-s-pomoshhyu-fileprovider/

--- app/src/main/kotlin/n7/myperfectemptyproject/utils/extension/Activities.kt
+++ app/src/main/kotlin/n7/myperfectemptyproject/utils/extension/Activities.kt
@@ -1,13 +1,24 @@
 package n7.myperfectemptyproject.utils.extension
 
 import android.app.Activity
+import android.content.Intent
 import android.net.Uri
 import androidx.core.app.ShareCompat
+import androidx.core.content.FileProvider
+import n7.myperfectemptyproject.BuildConfig
+import java.io.File
+
+fun Activity.getUri(file: File) {
+    FileProvider.getUriForFile(this, "${BuildConfig.APPLICATION_ID}.provider", file)
+}
 
 fun Activity.share(uri: Uri) {
     val intent = ShareCompat.IntentBuilder.from(this)
         .setType("audio/mp4")
         .setStream(uri)
         .createChooserIntent()
+        .addFlags(Intent.FLAG_GRANT_PERSISTABLE_URI_PERMISSION)
+        .addFlags(Intent.FLAG_GRANT_READ_URI_PERMISSION)
+        .addFlags(Intent.FLAG_GRANT_WRITE_URI_PERMISSION)
     startActivity(intent)
 }
