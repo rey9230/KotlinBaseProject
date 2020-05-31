f7c98fbc1b35c302ba8b14a1cabba8a5c890575c
i30mb1 <fate.i30mb1@gmail.com>
2020-05-31 14:57:30 +0300

    create fragment for feature one

---
+++ feature_one/src/main/java/ad2/n7/feature_one/FeatureOne.kt
@@ -0,0 +1,14 @@
+package ad2.n7.feature_one
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class FeatureOne: Fragment(R.layout.fragment_feature_one) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+    }
+
+}
