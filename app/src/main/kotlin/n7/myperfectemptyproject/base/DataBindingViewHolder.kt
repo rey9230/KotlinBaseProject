97460af163cd0e142c249b5c79af494898b83eb6
i30mb1 <fate.i30mb1@gmail.com>
2020-06-01 21:53:09 +0300

    add great base adapter for recyclerView from my France friend... https://proandroiddev.com/android-data-binding-listadapter-9e72ce50e8c7

---
+++ app/src/main/kotlin/n7/myperfectemptyproject/base/DataBindingViewHolder.kt
@@ -0,0 +1,14 @@
+package n7.myperfectemptyproject.base
+
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
+import n7.myperfectemptyproject.BR
+
+class DataBindingViewHolder<T>(private val binding: ViewDataBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    fun bind(item: T) {
+        binding.setVariable(BR.user, item)
+        binding.executePendingBindings()
+    }
+
+}
