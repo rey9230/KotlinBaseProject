94dea20086119b732e21f038b3a18573ab861d3d
i30mb1 <fate.i30mb1@gmail.com>
2020-06-04 16:31:18 +0300

    add utils method that can help in development

--- app/src/main/kotlin/n7/myperfectemptyproject/base/MarginItemDecorator.kt
+++ app/src/main/kotlin/n7/myperfectemptyproject/base/MarginItemDecorator.kt
@@ -4,14 +4,15 @@ import android.graphics.Rect
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 
-class MarginItemDecorator(private val spaceSize: Int): RecyclerView.ItemDecoration() {
+class MarginItemDecorator(private val spaceSize: Int?) : RecyclerView.ItemDecoration() {
     override fun getItemOffsets(outRect: Rect, view: View, parent: RecyclerView, state: RecyclerView.State) {
+        checkNotNull(spaceSize)
+        require(spaceSize > 0) { "Why are you gay?" }
         with(outRect) {
             top = spaceSize
             left = spaceSize
             right = spaceSize
-            bottom= spaceSize
+            bottom = spaceSize
         }
     }
-
 }
