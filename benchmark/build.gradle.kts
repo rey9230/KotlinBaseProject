281e651166ff14d22599c20ab575a2c81f6eaaf3
i30mb1 <fate.i30mb1@gmail.com>
2020-07-01 10:50:11 +0300

    update dependencies for Benchmark

--- benchmark/build.gradle.kts
+++ benchmark/build.gradle.kts
@@ -28,11 +28,9 @@ dependencies {
     // in an app module this way - you will need to move any code you
     // want to benchmark to a library module:
     // https://developer.android.com/studio/projects/android-library#Convert
+    implementation(kotlin("stdlib-jdk7"))
 
-    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.3.70")
-
-    androidTestImplementation("androidx.test:runner:1.2.0")
-    androidTestImplementation("androidx.test.ext:junit:1.1.1")
-    androidTestImplementation("junit:junit:4.12")
-    androidTestImplementation("androidx.benchmark:benchmark-junit4:1.0.0-alpha04")
+    androidTestImplementation(Lib.Test.testRunner)
+    androidTestImplementation(Lib.Test.testJunit)
+    androidTestImplementation("androidx.benchmark:benchmark-junit4:1.1.0-alpha01")
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -67,7 +67,6 @@ buildscript {
         classpath(kotlin("gradle-plugin", version = "1.3.72"))
         classpath("androidx.navigation:navigation-safe-args-gradle-plugin:${Versions.navigation}")
         classpath("androidx.benchmark:benchmark-gradle-plugin:1.0.0")
-        classpath("de.mannodermaus.gradle.plugins:android-junit5:1.5.2.0")
         // NOTE: Do not place your application dependencies here; they belong
         // in the individual module build.gradle files
     }
