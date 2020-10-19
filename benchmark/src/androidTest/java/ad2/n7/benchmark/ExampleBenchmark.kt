26c5f3daeca1d2fb03f22a0f44f00c2dcaafd22b
i30mb1 <fate.i30mb1@gmail.com>
2020-10-19 14:27:35 +0300

    add comments

--- benchmark/src/androidTest/java/ad2/n7/benchmark/ExampleBenchmark.kt
+++ benchmark/src/androidTest/java/ad2/n7/benchmark/ExampleBenchmark.kt
@@ -2,6 +2,7 @@ package ad2.n7.benchmark
 
 import android.content.Context
 import android.util.Log
+import android.util.TimingLogger
 import androidx.benchmark.junit4.BenchmarkRule
 import androidx.benchmark.junit4.measureRepeated
 import androidx.test.core.app.ApplicationProvider
@@ -9,12 +10,10 @@ import androidx.test.ext.junit.runners.AndroidJUnit4
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
+import kotlin.system.measureNanoTime
 
 /**
- * Benchmark, which will execute on an Android device.
- *
- * The body of [BenchmarkRule.measureRepeated] is measured in a loop, and Studio will
- * output the result. Modify your code to see how it affects performance.
+ * measureNanoTime {  }
  */
 
 /**
@@ -22,6 +21,18 @@ import org.junit.runner.RunWith
  * Trace.beginSection("onBind")
  * ...
  * Trace.endSection()
+ */
+
+/**
+ * Also we can use Kotlin exteinsion to measure time
+ * val time = measureNanoTime { ... }
+ */
+
+/**
+ * Benchmark, which will execute on an Android device.
+ *
+ * The body of [BenchmarkRule.measureRepeated] is measured in a loop, and Studio will
+ * output the result. Modify your code to see how it affects performance.
  */
 @RunWith(AndroidJUnit4::class)
 class ExampleBenchmark {
